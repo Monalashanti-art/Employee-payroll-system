16282c8ee7809b2d46c7457289a6ec16972373de
Monalashanti-art <lashanti2501@gmail.com>
2026-03-12 13:29:31 +0200

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,12 @@
+INSERT INTO all_hired 
+ (employee_id, first_name, last_name, department, salary, hire_date) 
+ VALUES (1,'Mona','Matlawe', 'Data Engineer', 40000, '2022-01-03'), 
+ (2,'Lindi','Hansen','Marketing',26500, '2019-12-01'), 
+ (3,'Kendal','Lewinsky','Lead Auditor', 35000, '2021-02-14'),
+ (4,'Neo','Molefe', 'Junior Data Analyst', 32500, '2025-02-01'),
+ (5,'Peter','Saaiman', 'Marketing', 37000, '2015-05-13'), 
+ (6,'Selena','Du pont', 'CEO', 85000, '2020-03-27'), 
+ (7,'Lashanti','Gray', 'Operations', 50000, '2018-10-19'), 
+ (8,'Kabelo', 'Ansante', 'IT', 36000, '2022-08-25'), 
+ (9,'Ziyanda','Louw','Laboratory', 40000, '2023-04-06'), 
+ (10,'Ariana', 'Duplessis', 'Mining', 60000, '2026-01-17');
